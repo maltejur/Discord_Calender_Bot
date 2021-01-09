83777c9e854c6dd4b4c6b0c2b60dc3e5c7b2dbb1
Calvin Katt <katt.calvin@gmail.com>
2021-01-09 12:39:23 +0100

    Continued Database

--- config/config.go
+++ config/config.go
@@ -8,9 +8,10 @@ import (
 
 //Config Struct to store config data from config.yaml
 type Config struct {
-	Token      string `yaml:"token"`
-	Logfile    string `yaml:"logfile"`
-	Validtypes string `yaml:"validtypes,flow"`
+	Token        string `yaml:"token"`
+	Logfile      string `yaml:"logfile"`
+	Validtypes   string `yaml:"validtypes,flow"`
+	DatabaseFile string `yaml:"databasefile"`
 }
 
 //ReadConfigFile parses file at path as yaml and returns result in Config struct
--- database/database.go
+++ database/database.go
@@ -29,10 +29,35 @@ func InitDB(path string) {
 
 //GetAppointmentsFromDatabase recieves all appointments from a given channel
 func GetAppointmentsFromDatabase(channelID string) []Appointment {
+	var aps []Appointment
+	sqlStmt := `SELECT * FROM "` + channelID + `";`
+	stmt, err := db.Prepare(sqlStmt)
 
 }
 
 //WriteAppointmentToDatabse writes Appointment to Database
 func WriteAppointmentToDatabse(channelID string, ap Appointment) error {
+	sqlStmt := `INSERT INTO "` + channelID + `" (
+		description,deadline,type
+	) VALUES (
+		?,?,?
+	);`
+	stmt, err := db.Prepare(sqlStmt)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(ap.description, ap.deadline.String(), ap.ty)
+	return err
+}
 
+//MakeNewChannelTable creates new table for new Channel
+func MakeNewChannelTable(channelID string) error {
+	sqlStmt := `CREATE TABLE IF NOT EXISTS "` + channelID + `"(
+		id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+		description TEXT,
+		deadline TEXT NOT NULL,
+		type TEXT NOT NULL);`
+	_, err := db.Exec(sqlStmt)
+	return err
 }
